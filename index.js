49ce58cd967c6b606a66a3dd414eb396f0716e62
WANGMENG521 <64994508+WANGMENG521@users.noreply.github.com>
2021-02-23 20:51:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-(async () => {unicom --user 15556716292 --password 521123 --appid bcbf9239dffadd5449045269ab5346e86d9fd3803a7fe6fbd3324eafbd2d706a742b0ba431ea5af8838ff0cca001e416fc8ccb762693d551275a1f52789d6a73605d31c95066a620bcea1aa1b7010815
+(async () => {
   require('./AutoSignMachine.js').run()
 })()
 
